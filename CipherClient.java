6bb2dcd0294f8a6359d3590dd3a176aafb6f811d
parusha <parusha.pradhan@gmail.com>
2025-03-01 21:38:15 -0500

    Starter files

---
+++ CipherClient.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+import java.security.*;
+import javax.crypto.*;
+
+public class CipherClient
+{
+	public static void main(String[] args) throws Exception 
+	{
+		String message = "The quick brown fox jumps over the lazy dog.";
+		String host = "your local ip address";
+		int port = 7999;
+		Socket s = new Socket(host, port);
+		
+		// YOU NEED TO DO THESE STEPS:
+		// -Generate a DES key.
+		// -Store it in a file.
+		// -Use the key to encrypt the message above and send it over socket s to the server.		
+
+	}
+}
